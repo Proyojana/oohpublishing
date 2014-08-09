770f73598558330f0dc0727b5a367242ba278171
Gowri sundari <gowri.sundari@proyojana.com>
2014-08-09 18:33:05 +0530

    Schedule

--- model/ViewSchedule.js
+++ model/ViewSchedule.js
@@ -2,7 +2,7 @@ Ext.define('MyDesktop.model.ViewSchedule', {
         extend: 'Ext.data.Model',
         fields: [
            
-              'schedule_id','schedule_stage','schedule_estimated_daysperstage','schedule_actual_daysperstage','schedule_estimated_start_date','schedule_actual_start_date','schedule_actual_end_date','schedule_bufferday'      
+              'schedule_id','schedule_stage','schedule_estimated_daysperstage','schedule_actual_daysperstage','schedule_estimated_start_date','schedule_actual_start_date','schedule_actual_end_date','schedule_bufferday','schedule_estimated_end_date'      
         ],
       //  idProperty: 'cityname'
     });
