9504c841f1e8254c7395e22a4dfa784732e964ec
Gowri sundari <gowri.sundari@proyojana.com>
2014-09-13 18:08:19 +0530

    Artwork bottom save & Icon

--- view/projectmanagement/newproject/artwork/newprojectArtworkForm.js
+++ view/projectmanagement/newproject/artwork/newprojectArtworkForm.js
@@ -61,6 +61,116 @@ Ext.define('MyDesktop.view.projectmanagement.newproject.artwork.newprojectArtwor
 			height:260
 		}
 		*/
+		
+		{
+				xtype:'button',
+				text:'Save',
+				pressed:true,
+				x:450,y:450,
+				width:100,
+				//	margin:'0 0 0 100',
+				handler: function() {
+					
+					
+					var total_cost=0;
+					var total_redraws=0;
+					var total_relabel=0;
+					var total_final=0;
+					/*** get value from store**/
+					var myStore = Ext.getCmp('createprojectArtworkgrid').getStore();
+					myStore.each(function(rec) {
+					
+				    total_cost=total_cost+parseInt(rec.get('cost'));
+				    
+				    total_redraws=total_redraws+parseInt(rec.get('redrawsimple'));
+				   
+				    total_relabel=total_relabel+parseInt(rec.get('relabel'));
+				    total_final=total_final+parseInt(rec.get('finalartwrk'));
+				    
+				   					
+				});
+				
+				Ext.getCmp('add_total_cost').setValue(total_cost);
+				Ext.getCmp('add_total_redraws').setValue(total_redraws);
+				Ext.getCmp('add_total_relabel').setValue(total_relabel);
+				Ext.getCmp('add_total_final').setValue(total_final);
+					
+					
+				//	var job_code=Ext.getCmp('job_code').getValue();
+					var project_id=Ext.getCmp('add_ArtworkHeader_projectID').getValue();
+
+				                   var figurenumber='';
+                    				var inputformat= '';
+                    				var resolution='';
+                 					var colourmode='';
+                    				var vendorassessment='';
+                    				var cnvrt='';
+                    				var redrawsimple='';
+                    				var redrawcomplex='';
+                    				var relabel='';                				
+                    				var finalartwrk='';
+                    				var cost='';
+                    				var comments='';
+                    				var artwork_id='';
+					var grid=Ext.getCmp('createprojectArtworkgrid');
+
+					var myStore = Ext.getCmp('createprojectArtworkgrid').getStore();
+					myStore.each( function(rec) {
+
+						figurenumber=figurenumber+rec.get('figurenumber')+',';
+						inputformat=inputformat+rec.get('inputformat')+',';
+						resolution=resolution+rec.get('resolution')+',';
+						colourmode=colourmode+rec.get('colourmode')+',';
+						vendorassessment=vendorassessment+rec.get('vendorassessment')+',';
+						cnvrt=cnvrt+rec.get('convert1')+',';
+						redrawsimple=redrawsimple+rec.get('redrawsimple')+',';
+						redrawcomplex=redrawcomplex+rec.get('redrawcomplex')+',';
+						relabel=relabel+rec.get('relabel')+',';
+						finalartwrk=finalartwrk+rec.get('finalartwrk')+',';
+						cost=cost+rec.get('cost')+',';
+						comments=comments+rec.get('comments')+',';
+						artwork_id=artwork_id+rec.get('id')+',';
+
+					});
+					var conn = new Ext.data.Connection();
+					conn.request({
+						url: 'service/Artwork.php',
+						method: 'POST',
+						params : {
+							action:2,
+							
+							project_id:project_id,
+							figurenumber:figurenumber,
+							inputformat:inputformat,
+							resolution:resolution,
+							colourmode:colourmode,
+							vendorassessment:vendorassessment,
+							cnvrt:cnvrt,
+							redrawsimple:redrawsimple,
+							redrawcomplex:redrawcomplex,
+							relabel:relabel,
+							finalartwrk:finalartwrk,
+							cost:cost,
+							comments:comments,
+							artwork_id:artwork_id
+						},
+						success: function(response) {
+							obj = Ext.JSON.decode(response.responseText);
+							Ext.Msg.alert('Message', obj.message);
+							//refresh grid
+							var grid3=Ext.getCmp('createprojectArtworkgrid');
+							grid3.getStore().load({
+								params: {
+									action:3,
+									project_id:project_id
+								}
+							});
+
+						}
+					});
+
+				}
+			},
 	
 			]
 	  
