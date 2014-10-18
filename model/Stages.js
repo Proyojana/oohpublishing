d7646a35655072c801dba0b149ff141113c0941a
balaji.p@proyojana.com <9043823281p>
2014-10-18 16:17:30 +0530

    tinymce

--- model/Stages.js
+++ model/Stages.js
@@ -1,8 +1,9 @@
  Ext.define('MyDesktop.model.Stages', {
         extend: 'Ext.data.Model',
         fields: [
-            
-             'stage_order','stage_id','stage_name','activity'          
+             
+             'stage_order','stage_id','stage_name','activity','no_of_days','ratecard_USD','ratecard_GBP'
+                       
         ],
        //idProperty: 'statename'
     });
