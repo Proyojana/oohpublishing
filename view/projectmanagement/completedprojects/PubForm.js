2e66e3bd60e59f3640838b6462081f505503c90b
balaji.p@proyojana.com <9043823281p>
2014-11-06 18:25:58 +0530

    alignment

--- view/projectmanagement/completedprojects/PubForm.js
+++ view/projectmanagement/completedprojects/PubForm.js
@@ -3,9 +3,6 @@ Ext.define('MyDesktop.view.projectmanagement.completedprojects.PubForm' ,{
     alias : 'widget.pubformCP',
    		id:'pubformCP',
     margin: '10 10 10 10',
-	layout: {
-                                type: 'absolute'
-                            },
 	
 	frame:true,
 	requires:['MyDesktop.view.projectmanagement.completedprojects.PublisherGrid','MyDesktop.view.projectmanagement.completedprojects.Author','MyDesktop.view.projectmanagement.completedprojects.budgetGrid','MyDesktop.view.projectmanagement.completedprojects.AddChargesGrid','MyDesktop.view.projectmanagement.completedprojects.scheduleGrid','MyDesktop.view.projectmanagement.completedprojects.ContribGrid','MyDesktop.view.projectmanagement.completedprojects.NotesGrid','MyDesktop.view.projectmanagement.completedprojects.details','MyDesktop.view.projectmanagement.completedprojects.scheduleForm'],
@@ -17,7 +14,7 @@ Ext.define('MyDesktop.view.projectmanagement.completedprojects.PubForm' ,{
     },
 
     defaultType: 'textfield',
-	
+	  layout:'border',
 	initComponent:function(){
 		//var heads = Ext.create('MyDesktop.store.HeadedBy');
 		//heads.load({params:{action: 5}});
