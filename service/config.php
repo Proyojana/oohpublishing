5ec9b2c62fd67031e10cd8a6f5967b67fae23053
proyojana <pzahir.it@gmail.com>
2014-11-26 12:37:10 +0530

    Config changes

--- service/config.php
+++ service/config.php
@@ -1,7 +1,7 @@
 <?php
-$hostname="localhost";	//Host name of the Server
-$username="root";	//User Name of the Server
-$password="";	//Password of the Server
+$hostname="166.62.8.8";	//Host name of the Server
+$username="oohpublishing";	//User Name of the Server
+$password="Ooh@2014";	//Password of the Server
 $dbname="oohpublishing";	//Database Name of the server
 $connect=mysql_pconnect("$hostname","$username","$password") or die("Unable to connect to SQL server");
 @mysql_select_db($dbname,$connect) or die("Unable to select database");
