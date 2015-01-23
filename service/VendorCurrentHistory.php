ca7c0a97fcddc32c520f981577304e21b319b98e
balaji.p@proyojana.com <9043823281p>
2015-01-23 19:22:42 +0530

    Changes in current and History

--- service/VendorCurrentHistory.php
+++ service/VendorCurrentHistory.php
@@ -14,57 +14,59 @@ $id=$_SESSION['id'];
 		default: 
 			break;
 	}
-	
 
 function getCurrentProjects($vendorid)
 	{
  		$num_result = mysql_query ("Select Distinct
-project_title.title As cur_proj_title,
-project_title.hb_isbn As cur_proj_hbisbn,
-project_title.pb_isbn As cur_proj_pbisbn,
-project_title.job_code As cur_proj_code,
-project_title.agreed_deadline As cur_proj_deadline,
-project_title.format,
-budget_expense.project_id,
-budget_total_detail.status,
-project_title.flag
+  project_title.title As cur_proj_title,
+  project_title.hb_isbn As cur_proj_hbisbn,
+  project_title.pb_isbn As cur_proj_pbisbn,
+  project_title.job_code As cur_proj_code,
+  project_title.agreed_deadline As cur_proj_deadline,
+  project_title.format,
+  budget_expense.project_id,
+  budget_total_detail.status,
+  project_title.flag
 From
-budget_expense Inner Join
-vendors On vendors.id = budget_expense.vendor
-Inner Join
-project_title On budget_expense.project_id =
-project_title.id Inner Join
-budget_total_detail On budget_expense.project_id =
-budget_total_detail.project_id
+  budget_expense Inner Join
+  vendors On vendors.id = budget_expense.vendor
+  Inner Join
+  project_title On budget_expense.project_id =
+    project_title.id Inner Join
+  budget_total_detail On budget_expense.project_id =
+    budget_total_detail.project_id
 Where
-budget_total_detail.status = 'Current' And
-project_title.flag = 0 And
-vendors.id ='".$vendorid."")or die(mysql_error());
+  budget_total_detail.status = 'Current' And
+  project_title.flag = 0 And
+  vendors.id ='".$vendorid."' ")or die(mysql_error());
 		
 		$totaldata = mysql_num_rows($num_result);
 
 		$result = mysql_query("Select Distinct
-project_title.title As cur_proj_title,
-project_title.hb_isbn As cur_proj_hbisbn,
-project_title.pb_isbn As cur_proj_pbisbn,
-project_title.job_code As cur_proj_code,
-project_title.agreed_deadline As cur_proj_deadline,
-project_title.format,
-budget_expense.project_id,
-budget_total_detail.status,
-project_title.flag
+  project_title.title As cur_proj_title,
+  project_title.hb_isbn As cur_proj_hbisbn,
+  project_title.pb_isbn As cur_proj_pbisbn,
+  project_title.job_code As cur_proj_code,
+  project_title.agreed_deadline As cur_proj_deadline,
+  project_title.format,
+  budget_expense.project_id,
+  budget_total_detail.status,
+  project_title.flag
 From
-budget_expense Inner Join
-vendors On vendors.id = budget_expense.vendor
-Inner Join
-project_title On budget_expense.project_id =
-project_title.id Inner Join
-budget_total_detail On budget_expense.project_id =
-budget_total_detail.project_id
+  budget_expense Inner Join
+  vendors On vendors.id = budget_expense.vendor
+  Inner Join
+  project_title On budget_expense.project_id =
+    project_title.id Inner Join
+  budget_total_detail On budget_expense.project_id =
+    budget_total_detail.project_id
 Where
-budget_total_detail.status = 'Current' And
-project_title.flag = 0 And
-vendors.id ='".$vendorid."'")or die(mysql_error());
+  budget_total_detail.status = 'Current' And
+  project_title.flag = 0 And
+  vendors.id ='".$vendorid."' ")or die(mysql_error());
+  
+  
+  
   
 		while($row=mysql_fetch_object($result))
 		{
@@ -73,56 +75,56 @@ vendors.id ='".$vendorid."'")or die(mysql_error());
 	   	echo'({"total":"'.$totaldata.'","results":'.json_encode($data).'})';
 	}   
 	
-	
+
 function getHistoryProjects($vendorid)
 	{
  		$num_result = mysql_query ("Select Distinct
-project_title.title As histry_proj_title,
-project_title.hb_isbn As histry_proj_hbisbn,
-project_title.pb_isbn As histry_proj_pbisbn,
-project_title.job_code As histry_proj_code ,
-project_title.agreed_deadline As histry_proj_deadline ,
-project_title.format,
-budget_expense.project_id,
-budget_total_detail.status,
-project_title.flag
+  project_title.title As histry_proj_title,
+  project_title.hb_isbn As histry_proj_hbisbn,
+  project_title.pb_isbn As histry_proj_pbisbn,
+  project_title.job_code As histry_proj_code ,
+  project_title.agreed_deadline As histry_proj_deadline ,
+  project_title.format,
+  budget_expense.project_id,
+  budget_total_detail.status,
+  project_title.flag
 From
-budget_expense Inner Join
-vendors On vendors.id = budget_expense.vendor
-Inner Join
-project_title On budget_expense.project_id =
-project_title.id Inner Join
-budget_total_detail On budget_expense.project_id =
-budget_total_detail.project_id
+  budget_expense Inner Join
+  vendors On vendors.id = budget_expense.vendor
+  Inner Join
+  project_title On budget_expense.project_id =
+    project_title.id Inner Join
+  budget_total_detail On budget_expense.project_id =
+    budget_total_detail.project_id
 Where
-budget_total_detail.status = 'Completed' And
-project_title.flag = 0 And
-vendors.id = '".$vendorid."'")or die(mysql_error());
+  budget_total_detail.status = 'Completed' And
+  project_title.flag = 0 And
+  vendors.id = '".$vendorid."'")or die(mysql_error());
 		
 		$totaldata = mysql_num_rows($num_result);
 
 		$result = mysql_query("Select Distinct
-project_title.title As histry_proj_title,
-project_title.hb_isbn As histry_proj_hbisbn,
-project_title.pb_isbn As histry_proj_pbisbn,
-project_title.job_code As histry_proj_code ,
-project_title.agreed_deadline As histry_proj_deadline ,
-project_title.format,
-budget_expense.project_id,
-budget_total_detail.status,
-project_title.flag
+  project_title.title As histry_proj_title,
+  project_title.hb_isbn As histry_proj_hbisbn,
+  project_title.pb_isbn As histry_proj_pbisbn,
+  project_title.job_code As histry_proj_code ,
+  project_title.agreed_deadline As histry_proj_deadline ,
+  project_title.format,
+  budget_expense.project_id,
+  budget_total_detail.status,
+  project_title.flag
 From
-budget_expense Inner Join
-vendors On vendors.id = budget_expense.vendor
-Inner Join
-project_title On budget_expense.project_id =
-project_title.id Inner Join
-budget_total_detail On budget_expense.project_id =
-budget_total_detail.project_id
+  budget_expense Inner Join
+  vendors On vendors.id = budget_expense.vendor
+  Inner Join
+  project_title On budget_expense.project_id =
+    project_title.id Inner Join
+  budget_total_detail On budget_expense.project_id =
+    budget_total_detail.project_id
 Where
-budget_total_detail.status = 'Completed' And
-project_title.flag = 0 And
-vendors.id = '".$vendorid."'")or die(mysql_error());
+  budget_total_detail.status = 'Completed' And
+  project_title.flag = 0 And
+  vendors.id = '".$vendorid."'")or die(mysql_error());
   
 		while($row=mysql_fetch_object($result))
 		{
