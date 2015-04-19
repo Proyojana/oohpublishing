61da92fc0a786752fe508d02811fcb6ca2745252
Proyojana <vanipriya@proyojana.com>
2015-04-19 10:23:17 +0530

    Download CSV Fixed
    Download Reports functionality has been added

--- view/projectmanagement/Reports/ProjectReportGrid.js
+++ view/projectmanagement/Reports/ProjectReportGrid.js
@@ -3,12 +3,10 @@ Ext.define('MyDesktop.view.projectmanagement.Reports.ProjectReportGrid', {
 	extend:'Ext.grid.Panel',
 	alias:'widget.projectreportgrid',
 	closeAction: 'hide',
-	
-	
 	height:200,
 	requires : ['MyDesktop.store.ProjectReport'],
 	title:'Project Reports',
-	id:'projectreportgrid',
+	
 	
 	initComponent: function() {
 		
@@ -133,7 +131,9 @@ Ext.define('MyDesktop.view.projectmanagement.Reports.ProjectReportGrid', {
 				
 		];
 		
-		this.bbar = Ext.create('Ext.PagingToolbar', {  
+
+		
+		this.bbar = Ext.create('Ext.Toolbar', {  
 
 			store : this.store,
 			displayInfo: true,
@@ -141,11 +141,14 @@ Ext.define('MyDesktop.view.projectmanagement.Reports.ProjectReportGrid', {
 			emptyMsg: "No topics to display",
 			 items:[
                        
-              /*         {
-                               xtype:'exporterbutton',
+                       {
                                text:'Export',
-                               margin:'0 0 0 40'
-                       },*/
+                               margin:'0 0 0 40',
+							   handler : function(){
+							   console.log(Report);
+							   JSONToCSVConvertor(Ext.encode(Ext.pluck(Report.data.items, 'data')), "OOH Project Report"+Date.now(), true);
+							   }
+                       },
                ]
  
 			
@@ -155,5 +158,78 @@ Ext.define('MyDesktop.view.projectmanagement.Reports.ProjectReportGrid', {
 	}
 });
 
-// Load first data page
-//    employee.loadPage(1);
+function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+console.log("Inside");
+    console.log(JSONData);
+
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    var CSV = '';    
+    //Set Report title in first row or line
+    
+    CSV += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (ShowLabel) {
+        var row = "";
+        
+        //This loop will extract the label from 1st index of on array
+        for (var index in arrData[0]) {
+            
+            //Now convert each value to string and comma-seprated
+            row += index + ',';
+        }
+
+        row = row.slice(0, -1);
+        
+        //append Label row with line break
+        CSV += row + '\r\n';
+    }
+    
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+        var row = "";
+        
+        //2nd loop will extract each column and convert it in string comma-seprated
+        for (var index in arrData[i]) {
+            row += '"' + arrData[i][index] + '",';
+        }
+
+        row.slice(0, row.length - 1);
+        
+        //add a line break after each row
+        CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {        
+        alert("Invalid data");
+        return;
+    }   
+    
+    //Generate a file name
+    var fileName = "MyReport_";
+    //this will remove the blank-spaces from the title and replace it with an underscore
+    fileName += ReportTitle.replace(/ /g,"_");   
+    
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+    
+    // Now the little tricky part.
+    // you can use either>> window.open(uri);
+    // but this will not work in some browsers
+    // or you will not get the correct file extension    
+    
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");    
+    link.href = uri;
+    
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    link.download = fileName + ".csv";
+    
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
